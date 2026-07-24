d2941d9709af9226142704998485082d0b642022
hamza khlaief <khlaiefhamza@gmail.com>
2026-07-24 11:18:21 +0100

    adding admin entity and their staff

---
+++ backend/app/user_management/domain/entities/student.py
@@ -0,0 +1,32 @@
+from user_management.domain.exceptions.domain_exception import InvalidEntityException
+from dataclasses import dataclass
+from datetime import datetime,timezone
+from uuid import UUID, uuid4
+
+@dataclass(frozen=True)
+class Student:
+    student_id: UUID
+    first_name: str
+    last_name: str
+    email: str
+    password:str
+    created_at: datetime
+    updated_at: datetime
+    establishment:str
+    registration_number:str
+    @classmethod
+    def create(cls, first_name: str, last_name: str, email: str, password:str, establishment:str, registration_number:str) -> 'Student':
+        if not first_name or not last_name or not email or not password or not establishment or not registration_number:
+            raise InvalidEntityException("All fields are required to create a Student.")
+        now = datetime.now(timezone.utc)
+        return cls(
+            student_id=uuid4(),
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            created_at=now,
+            updated_at=now,
+            establishment=establishment,
+            registration_number=registration_number
+        )
